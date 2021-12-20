a3282787dfa9816fe3eec4cc691e491457843809
pate0845 <pate0845@algonquinlive.com>
2021-12-20 12:30:59 -0500

    Game Completed

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let todoList = [];
 
+
+/*Run the method when button is clicked to check the input and 
+send the update message along with that run addTask method if successful*/
 function submitButton() {
   var input = document.getElementById("input").value;
   var spn = document.getElementById("span");
@@ -15,6 +18,9 @@ function submitButton() {
   }
 }
 
+
+/*Fetch the text from input element and push it to array,
+also clear the input field and show the update message*/
 function addTask() {
   var input = document.getElementById("input");
   todoList.push(input.value);
@@ -25,15 +31,19 @@ function addTask() {
   display();
 }
 
+
+/*Get the id of selected element and remove it from the array*/
 function removeElement(i) {
   todoList.splice(i, 1);
   display();
 }
 
+
+/*Display all the tasks using span element and updating continuously*/
 function display() {
   var span = document.getElementById("data");
   span.innerHTML = "";
   todoList.forEach(function (i, n) {
-    span.innerHTML += `<li id="userGoals">${i}<i class="material-icons" onclick=removeElement(${n}) id="delete-btn">delete</i></li>`;
+    span.innerHTML += `<li id="userGoals">${i}</li><i class="material-icons" onclick=removeElement(${n}) id="delete-btn">delete</i><hr>`;
   });
 }
